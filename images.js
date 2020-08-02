979014d4897122f117d75ff31c0a376d34d42810
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-08-02 21:13:30 +0200

    Tester ut litt

--- images.js
+++ images.js
@@ -1,27 +1,31 @@
-class Plaque{
-    constructor(mm, url){
-        this.mm = mm;
-        this.url = url;
+class Plaque {
+    constructor(mm, url) {
+      this.mm = mm;
+      this.url = url;
     }
-    calculateMm(){
-        if(this.mm>=3 && this.mm<6)
-        {
-            this.url = "assets/artery_plaque1.jpg";
-        }
-        else if(this.mm>=6)
-        {
-            this.url = "assets/artery_plaque2.jpg";
-        }
-        else
-        {
-            this.url = "assets/artery_clean.jpg";
-        }
-        document.getElementById("image").setAttribute("src", this.url);
+    calculateMm() {
+      const millimeter = this.mm;
+  
+      // Standardverdi hvis ingen av verdiene er true:
+      this.url = "assets/artery_clean.jpg";
+  
+      millimeter >= 3 &&
+        millimeter < 6 &&
+        (this.url = "assets/artery_plaque1.jpg");
+  
+      millimeter >= 6 && (this.url = "assets/artery_plaque2.jpg");
+  
+      document.getElementById("image").setAttribute("src", this.url);
+  
+      // Vis bilde når vi har noe å vise
+      // Fjern hidden klasse
+      document.getElementById("image").classList.remove("hidden");
     }
-}
-function myImages(){
+  }
+  function myImages() {
     let value = document.getElementById("plaque").value;
     console.log(value);
     let plaque = new Plaque(value, "#");
     plaque.calculateMm();
-}
+  }
+  
